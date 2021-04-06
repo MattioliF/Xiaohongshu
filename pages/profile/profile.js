a46f0dcdd110a4763d67c368d5c96fc32ef69137
Francisco Mattioli <franciscomattioli89@gmail.com>
2021-04-06 21:45:55 +0800

    added the profile page

---
+++ pages/profile/profile.js
@@ -0,0 +1,88 @@
+// pages/show/show.js
+const app = getApp()
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    currentUser: null,
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    console.log(app.globalData)
+    this.setData({
+      currentUser: app.globalData.userInfo
+    })
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  userInfoHandler: function(data) {
+    const self = this
+    wx.BaaS.auth.loginWithWechat(data).then(
+      (res) => {
+        console.log('results',res)
+        self.setData ({
+          currentUser:res
+        }),
+        wx.setStorageSync('userInfo', res)
+        getApp().globalData.userInfo = res
+      },
+      (err) => {
+      },
+    )
+  },
+
+})
